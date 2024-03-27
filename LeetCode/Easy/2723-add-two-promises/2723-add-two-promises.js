175a4123270d71466ff746d0c06d7b7f30b153d9
HICHULOG <115515471+HICHULOG@users.noreply.github.com>
2024-03-27 21:55:22 +0900

    Time: 61 ms (65.58%), Space: 49.3 MB (72.16%) - LeetHub

---
+++ LeetCode/Easy/2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,6 @@
+var addTwoPromises = async function(promise1, promise2) {
+    return Promise.all([promise1, promise2]).then(values => {
+        const sum = values[0] + values[1];
+        return sum;
+    });  
+};

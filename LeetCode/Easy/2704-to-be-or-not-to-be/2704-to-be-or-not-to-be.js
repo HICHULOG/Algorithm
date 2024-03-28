d28da5ff329eb7af51c80b4b51dbb66aca10e0ac
HICHULOG <115515471+HICHULOG@users.noreply.github.com>
2024-03-28 19:31:46 +0900

    Time: 55 ms (38.36%), Space: 48.9 MB (40.53%) - LeetHub

---
+++ LeetCode/Easy/2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,18 @@
+var expect = function(val) {
+    return {
+        toBe: function(checkVal) {
+            if (val === checkVal) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(checkVal) {
+            if (val !== checkVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+};

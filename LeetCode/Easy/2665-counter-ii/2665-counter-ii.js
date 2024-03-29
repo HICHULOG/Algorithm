f33b326e39ca2f9b474726beb099b76c0aa973ce
HICHULOG <115515471+HICHULOG@users.noreply.github.com>
2024-03-29 22:54:24 +0900

    Time: 53 ms (84.75%), Space: 51 MB (93.22%) - LeetHub

---
+++ LeetCode/Easy/2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,19 @@
+var createCounter = function(init) {
+    let currentValue = init;
+    
+    return {
+        increment: function () {
+            currentValue += 1;
+            return currentValue;
+        },
+        decrement: function () {
+            currentValue -= 1;
+            return currentValue;
+        },
+        reset: function () {
+            currentValue = init
+            return currentValue;
+        }
+        
+    } 
+};
